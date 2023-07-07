3e0c3ca41b0268ef1ea21de8420a611688f0f5ed
oscarwanga98 <87162511+oscarwanga98@users.noreply.github.com>
2023-07-07 13:23:00 +0300

    1st commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div>
+      <div>©{new Date().getFullYear()} Redleaf. All rights reserved</div>
+      <div></div>
+    </div>
+  );
+};
